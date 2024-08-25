261505f63b68eb1297798546fdb6c9e7a79fc760
GabrielMichelon2004 <gabriel.michelon@sptech.school>
2024-08-25 15:41:41 -0300

    primeiro commit

---
+++ desafio-plataforma-pagamentos/src/test/java/com/example/desafio_plataforma_pagamentos/DesafioPlataformaPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_plataforma_pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPlataformaPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
